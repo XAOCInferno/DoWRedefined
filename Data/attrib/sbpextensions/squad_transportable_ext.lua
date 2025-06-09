e48fefdc647510b07c583d7bd0a40c32f852b3e6
omar\Col <omarsarakbi206@gmail.com>
2025-06-09 21:54:34 +0300

    project upload Part 6

---
+++ Data/attrib/sbpextensions/squad_transportable_ext.lua
@@ -0,0 +1,4 @@
+GameData = Inherit([[]])
+GameData["activate_transport_ability"] = true
+GameData["nr_spots"] = 1
+GameData["transport_type"] = Reference([[type_transportable\transport_infantry.lua]])
